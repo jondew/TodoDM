fd56777af46a896b801a504921d066ed36fff92f
jondew <jon.wengde@gmail.com>
2023-12-11 15:17:07 +0100

    Partial implementation of TaskListAdapter

--- TodoMargotJonathan/app/src/main/java/com/margotjonathan/todo/list/TaskListAdapter.kt
+++ TodoMargotJonathan/app/src/main/java/com/margotjonathan/todo/list/TaskListAdapter.kt
@@ -1,5 +1,8 @@
 package com.margotjonathan.todo.list
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
 class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
     var currentList: List<String> = emptyList()
@@ -9,5 +12,18 @@ class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
         fun bind(taskTitle: String) {
             // on affichera les données ici
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.size
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
     }
 }
