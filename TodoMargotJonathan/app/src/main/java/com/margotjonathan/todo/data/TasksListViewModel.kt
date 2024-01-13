44a43aa22e6bb390c77ab4edcc46da5cdacd764b
jondew <jon.wengde@gmail.com>
2024-01-13 11:19:42 +0100

    TP3 Step 9: Added TasksListViewModel class

---
+++ TodoMargotJonathan/app/src/main/java/com/margotjonathan/todo/data/TasksListViewModel.kt
@@ -0,0 +1,31 @@
+package com.margotjonathan.todo.data
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.margotjonathan.todo.list.Task
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+class TasksListViewModel : ViewModel() {
+    private val webService = Api.tasksWebService
+
+    public val tasksStateFlow = MutableStateFlow<List<Task>>(emptyList())
+
+    fun refresh() {
+        viewModelScope.launch {
+            val response = webService.fetchTasks() // Call HTTP (opération longue)
+            if (!response.isSuccessful) { // à cette ligne, on a reçu la réponse de l'API
+                Log.e("Network", "Error: ${response.message()}")
+                return@launch
+            }
+            val fetchedTasks = response.body()!!
+            tasksStateFlow.value = fetchedTasks // on modifie le flow, ce qui déclenche ses observers
+        }
+    }
+
+    // à compléter plus tard:
+    fun add(task: Task) {}
+    fun edit(task: Task) {}
+    fun remove(task: Task) {}
+}
